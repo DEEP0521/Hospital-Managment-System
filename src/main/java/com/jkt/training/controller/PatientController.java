2c1a0bff8c342cc12584042c0c02acc3b2c41284
DEEPSHIKHA ROY <roymum05@gmail.com>
2020-05-07 01:26:30 +0530

    Patient Changes

--- src/main/java/com/jkt/training/controller/PatientController.java
+++ src/main/java/com/jkt/training/controller/PatientController.java
@@ -2,11 +2,15 @@ package com.jkt.training.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jkt.training.model.Patient;
@@ -31,5 +35,11 @@ public class PatientController {
 		}catch(Exception e) {
 			return new ResponseEntity<Patient>(HttpStatus.NOT_FOUND);
 		}
+	}
+	
+	@PostMapping(value = "/patient/save")
+	public int save(final @RequestBody @Valid Patient pt) {
+		Pservice.save(pt);
+		return pt.getPid();
 	}
 }
--- src/main/java/com/jkt/training/repository/PatientRepository.java
+++ src/main/java/com/jkt/training/repository/PatientRepository.java
@@ -1,9 +1,11 @@
 package com.jkt.training.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jkt.training.model.Patient;
 
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
+@Repository
+public interface PatientRepository extends CrudRepository<Patient, Integer> {
 
 }

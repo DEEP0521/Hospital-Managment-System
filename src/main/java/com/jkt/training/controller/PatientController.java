c9479c99b222b6238ed06141fa7e294549f0254e
DEEPSHIKHA ROY <roymum05@gmail.com>
2020-05-06 20:24:09 +0530

    Patient Entity Updated

--- src/main/java/com/jkt/training/controller/PatientController.java
+++ src/main/java/com/jkt/training/controller/PatientController.java
@@ -1,5 +1,35 @@
 package com.jkt.training.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jkt.training.model.Patient;
+import com.jkt.training.service.PatientService;
+
+@RestController
 public class PatientController {
 
+	@Autowired
+	private PatientService Pservice;
+	
+	@GetMapping("/patients") //provides list of all patients
+	public List<Patient> list(){
+		return Pservice.listAll();
+	}
+	
+	@GetMapping("/patients/{Pid}") //provides the details of selected patient
+	public ResponseEntity<Patient> get(@PathVariable Integer Pid){
+		try {
+			Patient pt=Pservice.get(Pid);
+			return new ResponseEntity<Patient>(pt,HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<Patient>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
--- src/main/java/com/jkt/training/model/Patient.java
+++ src/main/java/com/jkt/training/model/Patient.java
@@ -1,5 +1,93 @@
 package com.jkt.training.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Patient {
 
+	private int Pid;
+	private String Pname,Pdiagnosis,Paddress;
+	private int Record_id,Hospital_id;
+	
+	
+	public Patient() {
+		
+	}
+
+
+	public Patient(int pid, String pname, String pdiagnosis, String paddress, int record_id, int hospital_id) {
+		Pid = pid;
+		Pname = pname;
+		Pdiagnosis = pdiagnosis;
+		Paddress = paddress;
+		Record_id = record_id;
+		Hospital_id = hospital_id;
+	}
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getPid() {
+		return Pid;
+	}
+
+
+	public void setPid(int pid) {
+		Pid = pid;
+	}
+
+
+	public String getPname() {
+		return Pname;
+	}
+
+
+	public void setPname(String pname) {
+		Pname = pname;
+	}
+
+
+	public String getPdiagnosis() {
+		return Pdiagnosis;
+	}
+
+
+	public void setPdiagnosis(String pdiagnosis) {
+		Pdiagnosis = pdiagnosis;
+	}
+
+
+	public String getPaddress() {
+		return Paddress;
+	}
+
+
+	public void setPaddress(String paddress) {
+		Paddress = paddress;
+	}
+
+
+	public int getRecord_id() {
+		return Record_id;
+	}
+
+
+	public void setRecord_id(int record_id) {
+		Record_id = record_id;
+	}
+
+
+	public int getHospital_id() {
+		return Hospital_id;
+	}
+
+
+	public void setHospital_id(int hospital_id) {
+		Hospital_id = hospital_id;
+	}
+	
+	
 }

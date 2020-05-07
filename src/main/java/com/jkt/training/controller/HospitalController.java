076b56f7c9e79cb2d297bcaf49980d0d0efb6889
Srasheed <61048530+Sumayya-maven@users.noreply.github.com>
2020-05-08 00:27:37 +0530

    Update HospitalController.java

--- src/main/java/com/jkt/training/controller/HospitalController.java
+++ src/main/java/com/jkt/training/controller/HospitalController.java
@@ -29,7 +29,7 @@ public class HospitalController {
 		return hrservice.getById(hosp_id);
 	}
 	
-	@PostMapping(path="/hospitals")
+	@PostMapping("/hospitals")
 	public String addHospital(@RequestBody Hospital hospital)
 	{
 		hrservice.addHospital(hospital);
@@ -43,7 +43,7 @@ public class HospitalController {
 		return "The hospital with id "+ hosp_id +"is deleted.";
 	}
 	
-	@PutMapping(path = "/hospitals/{hosp_id}")
+	@PutMapping("/hospitals/{hosp_id}")
 	public String updateHospital(@RequestBody Hospital hospital,@PathVariable int hosp_id) {
 		hrservice.updateHospital(hospital, hosp_id);
 		return "Hospital is updated now.";

018a0dfca64006fc2feffdbfbbc4400ef00e65ac
Srasheed <61048530+Sumayya-maven@users.noreply.github.com>
2020-05-10 23:58:36 +0530

    Update HospitalRepository.java

--- src/main/java/com/jkt/training/repository/HospitalRepository.java
+++ src/main/java/com/jkt/training/repository/HospitalRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jkt.training.model.Hospital;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
+    public List findByDrId(int d_Id);
 }
